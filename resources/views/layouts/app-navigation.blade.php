8c22411e8983ab39e9a04660cd77ec3291f9f168
Michal Nosavcov <mnosavcov@gmail.com>
2024-05-02 19:22:47 +0200

    layout app

--- resources/views/layouts/app-navigation.blade.php
+++ resources/views/layouts/app-navigation.blade.php
@@ -11,9 +11,123 @@
                 </div>
 
                 <!-- Navigation Links -->
+                <div class="hidden sm:flex sm:items-center sm:ml-6">
+                    <x-dropdown align="left" width="48">
+                        <x-slot name="trigger">
+                            <button
+                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                <div>Projekty</div>
+
+                                <div class="ml-1">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                         viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd"
+                                              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                              clip-rule="evenodd"/>
+                                    </svg>
+                                </div>
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            <div class="grid grid-cols-[max-content_max-content]">
+                                <div>
+                                    <x-dropdown-content>
+                                        {{ __('Cenu navrhuje kupující') }}
+                                    </x-dropdown-content>
+
+                                    <x-dropdown-link :href="route('profile.edit')">
+                                        {{ __('Pozemky k pronájmu') }}
+                                    </x-dropdown-link>
+
+                                    <x-dropdown-link :href="route('profile.edit')">
+                                        {{ __('Pozemky na prodej') }}
+                                    </x-dropdown-link>
+
+                                    <x-dropdown-link :href="route('profile.edit')">
+                                        {{ __('Kapacita v síti distributora') }}
+                                    </x-dropdown-link>
+
+                                    <x-dropdown-link :href="route('profile.edit')">
+                                        {{ __('Postoupení práv k projektu') }}
+                                    </x-dropdown-link>
+
+                                    <x-dropdown-link :href="route('profile.edit')">
+                                        {{ __('Výstavba FVE na klíč') }}
+                                    </x-dropdown-link>
+                                </div>
+                                <div>
+                                    <x-dropdown-content>
+                                        {{ __('Cenu navrhuje prodávající') }}
+                                    </x-dropdown-content>
+
+                                    <x-dropdown-link :href="route('profile.edit')">
+                                        {{ __('Pozemky k pronájmu') }}
+                                    </x-dropdown-link>
+
+                                    <x-dropdown-link :href="route('profile.edit')">
+                                        {{ __('Pozemky na prodej') }}
+                                    </x-dropdown-link>
+
+                                    <x-dropdown-link :href="route('profile.edit')">
+                                        {{ __('Kapacita v síti distributora') }}
+                                    </x-dropdown-link>
+
+                                    <x-dropdown-link :href="route('profile.edit')">
+                                        {{ __('Postoupení práv k projektu') }}
+                                    </x-dropdown-link>
+
+                                    <x-dropdown-link :href="route('profile.edit')">
+                                        {{ __('Výstavba FVE na klíč') }}
+                                    </x-dropdown-link>
+                                </div>
+                                {{--                                <div>--}}
+                                {{--                                    <x-dropdown-content>--}}
+                                {{--                                        {{ __('Aukce') }}--}}
+                                {{--                                    </x-dropdown-content>--}}
+
+                                {{--                                    <x-dropdown-link :href="route('profile.edit')">--}}
+                                {{--                                        {{ __('Pozemky k pronájmu') }}--}}
+                                {{--                                    </x-dropdown-link>--}}
+
+                                {{--                                    <x-dropdown-link :href="route('profile.edit')">--}}
+                                {{--                                        {{ __('Pozemky na prodej') }}--}}
+                                {{--                                    </x-dropdown-link>--}}
+
+                                {{--                                    <x-dropdown-link :href="route('profile.edit')">--}}
+                                {{--                                        {{ __('Kapacita v síti distributora') }}--}}
+                                {{--                                    </x-dropdown-link>--}}
+
+                                {{--                                    <x-dropdown-link :href="route('profile.edit')">--}}
+                                {{--                                        {{ __('Postoupení práv k projektu') }}--}}
+                                {{--                                    </x-dropdown-link>--}}
+
+                                {{--                                    <x-dropdown-link :href="route('profile.edit')">--}}
+                                {{--                                        {{ __('Výstavba FVE na klíč') }}--}}
+                                {{--                                    </x-dropdown-link>--}}
+                                {{--                                </div>--}}
+                                {{--                            </div>--}}
+                                <div class="col-span-2"></div>
+                            </div>
+                        </x-slot>
+                    </x-dropdown>
+                </div>
+
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('homepage')" :active="request()->routeIs('homepage')">
-                        {{ __('Homepage') }}
+                        {{ __('Jak to funguje') }}
+                    </x-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('homepage')" :active="request()->routeIs('homepage')">
+                        {{ __('O-nas') }}
+                    </x-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('homepage')" :active="request()->routeIs('homepage')">
+                        {{ __('Kontakt') }}
                     </x-nav-link>
                 </div>
             </div>
