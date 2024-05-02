8edf47768c0219d436ddb8c7cc56c6ec7e90b658
Michal Nosavcov <mnosavcov@gmail.com>
2024-05-02 17:00:23 +0200

    fornfamily: spartan

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,17 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Spartan-Regular, Nunito', ...defaultTheme.fontFamily.sans],
+                'Spartan-ExtraLight': ['Spartan-ExtraLight', ...defaultTheme.fontFamily.sans],
+                'Spartan-Bold': ['Spartan-Bold', ...defaultTheme.fontFamily.sans],
+                'Spartan-Black': ['Spartan-Black', ...defaultTheme.fontFamily.sans],
+                'Spartan-ExtraBold': ['Spartan-ExtraBold', ...defaultTheme.fontFamily.sans],
+                'Spartan-Thin': ['Spartan-Thin', ...defaultTheme.fontFamily.sans],
+                'Spartan-SemiBold': ['Spartan-SemiBold', ...defaultTheme.fontFamily.sans],
+                'Spartan-Medium': ['Spartan-Medium', ...defaultTheme.fontFamily.sans],
+                'Spartan-Light': ['Spartan-Light', ...defaultTheme.fontFamily.sans],
+                'Spartan-Regular': ['Spartan-Regular', ...defaultTheme.fontFamily.sans],
+                'Spartan-Spartan': ['Spartan-Spartan', ...defaultTheme.fontFamily.sans],
             },
         },
     },
