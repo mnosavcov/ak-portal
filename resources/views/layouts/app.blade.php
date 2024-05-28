3d5a0db8624f7b7a77fc72f516cdf11c4ca31fbf
Michal Nosavcov <mnosavcov@gmail.com>
2024-05-28 09:57:34 +0200

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,15 @@
     <!-- Scripts -->
     @vite(['resources/css/app.less', 'resources/js/app.js'])
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+    <link rel="manifest" href="/favicons/site.webmanifest">
+    <link rel="shortcut icon" href="/favicons/favicon.ico">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+
     <script src="/js/tinymce/tinymce.min.js"></script>
 </head>
 <body class="font-sans antialiased bg-[#f8f8f8] text-[#31363A]">
